3528cdb3590a6a814c28d16b7ac7545528640d6d
Pablo Olmos <paguirre@illumina.com>
2018-03-23 16:35:10 +0000

    Bad code, again

--- stormyweather/main.py
+++ stormyweather/main.py
@@ -1,5 +1,9 @@
 
-if __name__ == '__main__':
+def amazing_foo():
     print('Hi I am new around here')
     print('Get right down to the real nitty gritty!')
     raise ValueError('Boom!')
+
+
+if __name__ == '__main__':
+    amazing_foo()
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,4 +4,5 @@ import unittest
 class TestMain(unittest.TestCase):
 
     def test_should_run_without_raising_error(self):
-        pass
+        try:
+            the_
