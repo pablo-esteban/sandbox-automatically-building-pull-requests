925c24ee376cf0dd7ea741a3b886c00d5df4373b
Pablo Olmos <paguirre@illumina.com>
2018-03-23 16:21:31 +0000

    Not bad code

--- stormyweather/main.py
+++ stormyweather/main.py
@@ -1,5 +1,4 @@
 
-
-print('Hi I am new around here')
-print('Get right down to the real nitty gritty!')
-raise ValueError('Boom!')
+if __name__ == '__main__':
+    print('Hi I am new around here')
+    print('Get right down to the real nitty gritty!')
