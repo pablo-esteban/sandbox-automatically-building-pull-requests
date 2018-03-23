d30ab902388f539aa474226c1ac7b2fbdcde6251
Pablo Olmos <paguirre@illumina.com>
2018-03-23 15:44:50 +0000

    Created the most powerful foo and implemented test.

--- stormyweather/main.py
+++ stormyweather/main.py
@@ -1,3 +1,4 @@
 
-print('Hi I am new around here')
-print('Get right down to the real nitty gritty!')
+def the_most_powerful_foo():
+    print('Hi I am new around here')
+    print('Get right down to the real nitty gritty!')
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,12 @@
 import unittest
 
+from stormyweather.main import the_most_powerful_foo
+
 
 class TestMain(unittest.TestCase):
 
     def test_should_run_without_raising_error(self):
-        pass
+        try:
+            the_most_powerful_foo()
+        except Exception as e:
+            self.fail(str(e))
